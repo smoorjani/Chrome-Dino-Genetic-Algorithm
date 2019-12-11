bdda97806accc646095b0b3a32b5d2b05a129ece
Samraj Moorjani <samrajmoorjani@gmail.com>
2019-12-11 16:53:47 -0600

    Moved around files to have the game work.

--- fantastic-final/src/main.cpp
+++ fantastic-final/src/main.cpp
@@ -1,12 +1,36 @@
+/*
+Instructions:
+
+	To run the game:
+		- Uncomment the top block of code.
+		- Comment bottom block of code
+		- Remove all tests from the project
+
+	To run the tests:
+		- Comment top block of code
+		- Uncomment bottom block of code
+		- Add all tests back into project
+
+*/
+
+//========================================================================= 
+
 #include "ofMain.h"
 #include "ofApp.h"
 
-//========================================================================
 int main() {
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(1024, 768, OF_WINDOW);
 	ofSetFrameRate(60);
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
 	ofRunApp(new ofApp());
 }
+
+//========================================================================= 
+
+/*
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_FAST_COMPILE
+#include "C:\Users\smoor\source\repos\CS126FA19\fantastic-final\fantastic-final\catch.hpp"
+
+#include "../ofApp.h"
+#include "../ofMain.h"
+*/
