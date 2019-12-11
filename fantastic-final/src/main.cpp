f27625808a4a0c655c0cda433cfa3ef9f587f26e
Samraj Moorjani <samrajmoorjani@gmail.com>
2019-12-11 15:46:20 -0600

    Moving files.

--- fantastic-final/src/main.cpp
+++ fantastic-final/src/main.cpp
@@ -1,6 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_FAST_COMPILE
-#include "../src/catch.hpp"
+#include "catch.hpp"
 
 #include "../ofApp.h"
 #include "../ofMain.h"
+
+TEST_CASE("test blash") {
+	REQUIRE(1==1);
+}
